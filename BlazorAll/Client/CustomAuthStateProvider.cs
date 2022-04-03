3530e846259f4984590d3b7d45e0abb9c1502647
s4lish <s4lish@gmail.com>
2022-04-03 14:27:10 +0430

    add auth and roles in client side

---
+++ BlazorAll/Client/CustomAuthStateProvider.cs
@@ -0,0 +1,43 @@
+﻿using System.Security.Claims;
+using System.Text.Json;
+
+namespace BlazorAll.Client;
+
+public class CustomAuthStateProvider : AuthenticationStateProvider
+{
+    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        string token = "eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTUxMiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoic3RyaW5nIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjoiQWRtaW4iLCJleHAiOjE2NDcyNjE1MDl9.zAPwArMdu4n-u_pg_5SMDQ7J5QhA-JkHUL74ENVEKa730O4ch9WHtjXLEX5jCl8aJd330VsPAlvMaU7MifXAYQ";
+
+        var identity = new ClaimsIdentity(ParseClaimsFromJwt(token), "jwt");
+
+        //var identity = new ClaimsIdentity();
+        var user = new ClaimsPrincipal(identity);
+        var state = new AuthenticationState(user);
+
+        NotifyAuthenticationStateChanged(Task.FromResult(state));
+
+        return state;
+    }
+
+    public static IEnumerable<Claim> ParseClaimsFromJwt(string jwt)
+    {
+        var payload = jwt.Split('.')[1];
+        var jsonBytes = ParseBase64WhitoutPadding(payload);
+        var keyvaluePairs = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonBytes);
+        return keyvaluePairs.Select(kvp => new Claim(kvp.Key,kvp.Value.ToString()));
+
+    }
+
+    private static byte[] ParseBase64WhitoutPadding(string base64)
+    {
+        switch(base64.Length % 4)
+        {
+            case 2: base64 += "=="; break;
+            case 3: base64 += "="; break;
+        }
+
+        return Convert.FromBase64String(base64);
+    }
+}
+
--- BlazorAll/Client/Program.cs
+++ BlazorAll/Client/Program.cs
@@ -1,4 +1,5 @@
 global using BlazorAll.Shared.Models;
+global using Microsoft.AspNetCore.Components.Authorization;
 
 using BlazorAll.Client;
 using BlazorAll.Client.Services.SuperHeroService;
@@ -13,5 +14,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddScoped<ISuperHeroService, SuperHeroService>();
+builder.Services.AddScoped<AuthenticationStateProvider,CustomAuthStateProvider>();
+builder.Services.AddAuthorizationCore();
 
 await builder.Build().RunAsync();
